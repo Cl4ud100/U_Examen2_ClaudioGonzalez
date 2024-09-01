286c5298753c913a64ab28c0d86831a898a43437
Cl4ud100 <claudio.gonzalezg@uniacc.edu>
2024-09-01 18:25:42 -0400

    Code Door

--- Assets/Scripts/DoorPueblo.cs
+++ Assets/Scripts/DoorPueblo.cs
@@ -1,16 +1,39 @@
+using System;
 using UnityEngine;
 
 public class DoorPueblo : MonoBehaviour
 {
+    [Header("Door")] 
+    [SerializeField] private bool openDoor;
+    private Animator animDoor;
+    [SerializeField] private GameObject Door;
+    private Collider doorCollider;
+    [SerializeField] private GameObject doorColliderGameObject;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animDoor = Door.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "Door":
+            {
+                openDoor = true;
+                animDoor.SetBool("AnimDoor",true);
+                Debug.Log("Bienvenido a tus pesadillas");
+                break;
+            }
+        }
+
     }
 }
